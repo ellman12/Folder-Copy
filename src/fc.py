24e6975c7577216f92abbcbdb85cc26f508d53af
Elliott DuCharme <ellduc4@gmail.com>
2021-04-06 20:22:06 -0500

    cp, add, ls, and clr cmds work

--- src/fc.py
+++ src/fc.py
@@ -1,7 +1,9 @@
 import sys
 import os
 
-# def copyDirs(dirs, location):
+
+def copyDirs(dirs, location):
+    print("copy ")
 
 
 def listDirs(dirs):
@@ -18,32 +20,38 @@ with open(os.environ["APPDATA"] + "/fc/fc.conf", 'r') as conf:
 
 with open(os.environ["APPDATA"] + "/fc/dirs.txt", 'r') as dirstxt:
     dirs = [line.rstrip() for line in dirstxt]
+    dirstxt.close()
 
 if (len(sys.argv) == 1):
-    # TODO: copy()
+    copyDirs()
+    exit()
+
+elif (sys.argv[1] == "cp"):
+    copyDirs()
     exit()
 
 elif (sys.argv[1] == "add"):
     try:
+        for dir in dirs:  # Check for duplicates
+            if sys.argv[2] == dir:
+                print(f"{sys.argv[2]} is already in the file!")
+                exit()
+
         dirs.append(sys.argv[2])
+        with open(os.environ["APPDATA"] + "/fc/dirs.txt", 'a') as dirstxt:
+            for dir in dirs:
+                dirstxt.write(dir + '\n')
     except IndexError:
-        print("add requires a directory")
-    # finally:
-        # exit()
+        print("'add' requires a directory")
+        exit()
 
 elif (sys.argv[1] == "ls"):
     listDirs(dirs)
     exit()
 
-elif (sys.argv[1] == "clr") or (sys.argv[1] == "clear"):
+elif (sys.argv[1] == "clr"):
     with open(os.environ["APPDATA"] + "/fc/dirs.txt", 'w') as dirstxt:
         dirstxt.write("")
 
 else:
-    print(f"Unknown argument: '{sys.argv[1]}'")
-
-listDirs(dirs)
-
-with open(os.environ["APPDATA"] + "/fc/dirs.txt", 'a') as dirstxt:
-    for dir in dirs:
-        dirstxt.write(dir + '\n')
+    print(f"Unknown command: '{sys.argv[1]}'")
